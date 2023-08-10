d76566a781160bcae30730a416f6fa057d386db9
QudsiaSiddiqui <127615677+QudsiaSiddiqui@users.noreply.github.com>
2023-08-10 08:56:08 +0530

    This repository contains a simple web application that simulates a dice game. The game involves two players who each roll a dice, and the player with the higher dice value wins. In case of a tie, the game is declared a draw

---
+++ MyCodingProjects/Web development/JavaScript1.js
@@ -0,0 +1,18 @@
+﻿var num = Math.floor(Math.random() * 6) + 1;
+var diceimg = "D:/MyCodingProjects/Web development/images/dice" + num + ".png";
+var imgElement = document.querySelectorAll("img")[0];
+imgElement.setAttribute("src", diceimg);
+
+var num1 = Math.floor(Math.random() * 6) + 1;
+var diceimg1 = "D:/MyCodingProjects/Web development/images/dice" + num1 + ".png";
+var imgElement1 = document.querySelectorAll("img")[1];
+imgElement1.setAttribute("src", diceimg1);
+
+if (num > num1) {
+    document.querySelector("h1").innerHTML = "Player1 wins";
+}
+else if (num1 > num) {
+    document.querySelector("h1").innerHTML = "Player2 wins";
+} else if (num = num1) {
+    document.querySelector("h1").innerHTML = "DRAW";
+}
